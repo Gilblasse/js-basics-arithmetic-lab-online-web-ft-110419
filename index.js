30a76bdba94c09720c71a9318712ff9dd35be81a
Nethelbert <gilblasse@gmail.com>
2020-02-18 04:45:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let var1 = 31 
-let var2 = 2 
-const multiply = var1 * var2
+let var1 = 31;
+let var2 = 2;
+const multiply = var1 * var2;
 
-const random = Math.floor(Math.random() * 10)
+const random = Math.floor(Math.random() * 10);
